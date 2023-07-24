26cfb57d4fecc38dad807f07ff1382ad8335060f
nikola <nikolapantelic998@gmail.com>
2023-07-24 17:06:42 +0200

    Goals card redesign

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/home/GoalCard.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/home/GoalCard.js
@@ -22,38 +22,105 @@ export default function GoalCard() {
                     </Button>
                 </Typography>
 
-                <hr className="my-2 border-blue-gray-50 mb-8" />
+                <hr className="my-2 border-blue-gray-50 mb-6" />
 
                 <div>
                     <Typography variant="h6" className="text-gray-900">
-                        Vacation
+                        Weekly
                     </Typography>
-                    <div className="flex items-center justify-between mb-2">
-                        <Typography className="text-gray-900 font-medium mt-2">
-                            Week
-                        </Typography>
-                        <Typography className="text-gray-900 font-semibold mt-2">
-                            $40,00
-                        </Typography>
+                    <div className="mt-2">
+                        <div className="flex items-center justify-between mb-2">
+                            <Typography className="text-gray-900 font-medium mt-2">
+                                Laptop
+                            </Typography>
+                            <div className="flex gap-4 items-center">
+                                <Typography className="text-gray-900 font-semibold mt-2">
+                                    $20,00
+                                </Typography>
+                                <Typography className="text-gray-700 font-medium mt-2 text-sm">
+                                    80%
+                                </Typography>
+                            </div>
+                        </div>
+                        <Progress value={80} size="lg" className="mt-2" color="orange" />
                     </div>
-                    <Progress value={65} size="lg" className="mt-2" color="green" />
+
+                    <div className="mt-2">
+                        <div className="flex items-center justify-between mb-2">
+                            <Typography className="text-gray-900 font-medium mt-2">
+                                Aqua Park
+                            </Typography>
+                            <div className="flex gap-4 items-center">
+                                <Typography className="text-gray-900 font-semibold mt-2">
+                                    $0,00
+                                </Typography>
+                                <Typography className="text-gray-700 font-medium mt-2 text-sm">
+                                    100%
+                                </Typography>
+                            </div>
+                        </div>
+                        <Progress value={100} size="lg" className="mt-2" color="green" />
+                    </div>
                 </div>
 
-                <hr className="my-2 border-blue-gray-50 mb-8 mt-8" />
+                <hr className="my-2 border-blue-gray-50 mb-6 mt-6" />
 
                 <div>
                     <Typography variant="h6" className="text-gray-900">
-                        Car
+                        Monthly
                     </Typography>
-                    <div className="flex items-center justify-between mb-2">
-                        <Typography className="text-gray-900 font-medium mt-2">
-                            Month
-                        </Typography>
-                        <Typography className="text-gray-900 font-semibold mt-2">
-                            $400,00
-                        </Typography>
+                    <div className="mt-2">
+                        <div className="flex items-center justify-between mb-2">
+                            <Typography className="text-gray-900 font-medium mt-2">
+                                Vacation
+                            </Typography>
+                            <div className="flex gap-4 items-center">
+                                <Typography className="text-gray-900 font-semibold mt-2">
+                                    $100,00
+                                </Typography>
+                                <Typography className="text-gray-700 font-medium mt-2 text-sm">
+                                    40%
+                                </Typography>
+                            </div>
+                        </div>
+                        <Progress value={40} size="lg" className="mt-2" color="orange" />
                     </div>
-                    <Progress value={20} size="lg" className="mt-2" color="green" />
+
+                    <div className="mt-2">
+                        <div className="flex items-center justify-between mb-2">
+                            <Typography className="text-gray-900 font-medium mt-2">
+                                House
+                            </Typography>
+                            <div className="flex gap-4 items-center">
+                                <Typography className="text-gray-900 font-semibold mt-2">
+                                    $450,00
+                                </Typography>
+                                <Typography className="text-gray-700 font-medium mt-2 text-sm">
+                                    10%
+                                </Typography>
+                            </div>
+                        </div>
+                        <Progress value={10} size="lg" className="mt-2" color="red" />
+                    </div>
+                </div>
+
+                <hr className="my-2 border-blue-gray-50 mb-6 mt-6" />
+
+                <div className="container mx-auto mt-6">
+                    <p className="text-xs flex items-center justify-center gap-4">
+                        <span className="flex items-center">
+                            <span className="w-3 h-3 inline-block mr-1 bg-green-500"></span>
+                            <span className="text-xxs">Complete</span>
+                        </span>
+                        <span className="flex items-center">
+                            <span className="w-3 h-3 inline-block mr-1 bg-orange-500"></span>
+                            <span className="text-xxs">In progress</span>
+                        </span>
+                        <span className="flex items-center">
+                            <span className="w-3 h-3 inline-block mr-1 bg-red-500"></span>
+                            <span className="text-xxs">Not started</span>
+                        </span>
+                    </p>
                 </div>
 
                 <div className="h-8"></div>
