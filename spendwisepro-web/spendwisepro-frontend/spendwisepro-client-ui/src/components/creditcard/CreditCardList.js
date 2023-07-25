5d98fe652899c4d025b5156c0d7d4a30b3a24617
nikola <nikolapantelic998@gmail.com>
2023-07-25 16:25:04 +0200

    List of credit cards update

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/creditcard/CreditCardList.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/creditcard/CreditCardList.js
@@ -6,13 +6,12 @@ import {
 } from "@material-tailwind/react";
 import {ArrowLongRightIcon, PencilSquareIcon} from "@heroicons/react/24/outline";
 import React from "react";
-import {CreditCardIcon} from "@heroicons/react/24/solid";
 
 export default function CreditCardList() {
     return (
         <Card className="w-full shadow-lg mt-8">
             <CardBody>
-                <Typography variant="h4" color="blue-gray" className="mb-4 flex items-center justify-between">
+                <Typography variant="h5" color="blue-gray" className="mb-4 flex items-center justify-between">
                     Your Credit Cards
                     <Button size="sm" variant="text" className="flex gap-2">
                         <a>
@@ -22,54 +21,86 @@ export default function CreditCardList() {
                     </Button>
                 </Typography>
 
-                <hr className="my-2 border-blue-gray-50 mb-8" />
+                <hr className="my-2 border-blue-gray-50" />
 
-                <div className="mb-2 flex items-center justify-between">
-                    <div>
-                        <Typography variant="h5" className="text-gray-900 mb-2 mt-2 ml-8">
-                            <span className="text-lg font-medium text-gray-900 truncate">VISA</span>
-                        </Typography>
+                <div className="flow-root">
+                    <ul role="list" className="divide-y divide-gray-200">
 
-                        <Typography variant="h4" className="text-gray-900 font-semibold mt-2 ml-8 mb-2">
-                            $70,00
-                        </Typography>
-                    </div>
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center space-x-4">
+                                <div className="flex-shrink-0">
+                                    <img className="w-8 h-8 rounded-full" src="https://cdn3.iconfinder.com/data/icons/popular-services-brands-vol-2/512/visa-512.png" />
+                                </div>
+                                <div className="flex-1 min-w-0">
+                                    <p className="text-md font-medium text-gray-900 truncate dark:text-white">
+                                        Visa
+                                    </p>
+                                    <p className="text-sm text-gray-500 truncate">
+                                        Banka Intesa
+                                    </p>
+                                    <p className="text-sm text-gray-500 truncate">
+                                        Some Note1
+                                    </p>
+                                </div>
+                                <div className="text-right">
+                                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                        $120,00
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
 
-                    <div className=" items-center text-base font-semibold text-gray-900 dark:text-white mb-4 size">
-                        <Button size="lg" variant="text" className="flex gap-2">
-                            <a>
-                                Edit
-                            </a>
-                            <PencilSquareIcon strokeWidth={2} className="w-4 h-4" />
-                        </Button>
-                    </div>
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center space-x-4">
+                                <div className="flex-shrink-0">
+                                    <img className="w-8 h-8 rounded-full" src="https://cdn3.iconfinder.com/data/icons/circle-payment-methods-4/512/Mastercard-512.png" />
+                                </div>
+                                <div className="flex-1 min-w-0">
+                                    <p className="text-md font-medium text-gray-900 truncate">
+                                        MasterCard
+                                    </p>
+                                    <p className="text-sm text-gray-500 truncate">
+                                        OTP Banka
+                                    </p>
+                                    <p className="text-sm text-gray-500 truncate">
+                                        Some Note2
+                                    </p>
+                                </div>
+                                <div className="text-right">
+                                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                                        $50,00
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center space-x-4">
+                                <div className="flex-shrink-0">
+                                    <img className="w-8 h-8 rounded-full" src="https://cdn3.iconfinder.com/data/icons/popular-services-brands-vol-2/512/american-express-512.png" />
+                                </div>
+                                <div className="flex-1 min-w-0">
+                                    <p className="text-md font-medium text-gray-900 truncate">
+                                        American Express
+                                    </p>
+                                    <p className="text-sm text-gray-500 truncate">
+                                        U.S. Bank
+                                    </p>
+                                    <p className="text-sm text-gray-500 truncate">
+                                        Some Note3
+                                    </p>
+                                </div>
+                                <div className="text-right">
+                                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                                        $130,00
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                    </ul>
                 </div>
 
-
-                <hr className="my-2 border-blue-gray-50 mb-8 mt-8" />
-
-                <div className="mb-2 flex items-center justify-between">
-                    <div>
-                        <Typography variant="h5" className="text-gray-900 mb-2 mt-2 ml-8">
-                            <span className="text-lg font-medium text-gray-900 truncate">MASTERCARD</span>
-                        </Typography>
-
-                        <Typography variant="h4" className="text-gray-900 font-semibold mt-2 ml-8 mb-2">
-                            $230,00
-                        </Typography>
-                    </div>
-
-                    <div className=" items-center text-base font-semibold text-gray-900 dark:text-white mb-4 size">
-                        <Button size="lg" variant="text" className="flex gap-2">
-                            <a>
-                                Edit
-                            </a>
-                            <PencilSquareIcon strokeWidth={2} className="w-4 h-4" />
-                        </Button>
-                    </div>
-                </div>
-
-                <div className="h-8"></div>
             </CardBody>
         </Card>
     );
