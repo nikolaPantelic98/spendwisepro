772d4a7ec7c279aeaa87c7b4d80e7f988b1d1cea
nikola <nikolapantelic998@gmail.com>
2023-07-26 17:19:17 +0200

    Arrow added to the budget card on the home page

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/home/BudgetCard.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/home/BudgetCard.js
@@ -4,9 +4,10 @@ import {
     Typography,
     Button,
 } from "@material-tailwind/react";
-import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
+import {ArrowLongRightIcon, ChevronRightIcon} from "@heroicons/react/24/outline";
 import React from "react";
 import { Progress } from "@material-tailwind/react";
+import {Link} from "react-router-dom";
 
 export default function BudgetCard() {
     return (
@@ -25,84 +26,116 @@ export default function BudgetCard() {
                 <hr className="my-2 border-blue-gray-50 mb-6" />
 
                 <div>
-                    <Typography variant="h6" className="text-gray-900">
+                    <Typography variant="h6" className="text-gray-900 mb-2">
                         Weekly
                     </Typography>
-                    <div className="mt-2">
-                        <div className="flex items-center justify-between mb-2">
-                            <Typography className="text-gray-900 font-medium mt-2">
-                                General
-                            </Typography>
-                            <div className="flex gap-4 items-center">
-                                <Typography className="text-gray-900 font-semibold mt-2">
-                                    $70,00
-                                </Typography>
-                                <Typography className="text-gray-700 font-medium mt-2 text-sm">
-                                    50%
-                                </Typography>
-                            </div>
-                        </div>
-                        <Progress value={50} size="lg" className="mt-2" color="green" />
-                    </div>
 
-                    <div className="mt-2">
-                        <div className="flex items-center justify-between mb-2">
-                            <Typography className="text-gray-900 font-medium mt-2">
-                                Tobacco
-                            </Typography>
-                            <div className="flex gap-4 items-center">
-                                <Typography className="text-gray-900 font-semibold mt-2">
-                                    -$5,00
-                                </Typography>
-                                <Typography className="text-gray-700 font-medium mt-2 text-sm">
-                                    -10%
-                                </Typography>
+                    <Link>
+                        <div className="mb-2 flex items-center">
+                            <div className="flex-1 w-0">
+                                <div className="flex items-center justify-between mb-2">
+                                    <Typography className="text-gray-900 font-medium mt-2 truncate">
+                                        General
+                                    </Typography>
+                                    <div className="flex gap-4 items-center">
+                                        <Typography className="text-gray-900 font-semibold mt-2">
+                                            $70,00
+                                        </Typography>
+                                        <Typography className="text-gray-700 font-medium mt-2 text-sm">
+                                            50%
+                                        </Typography>
+                                    </div>
+                                </div>
+                                <Progress value={50} size="lg" className="mt-2" color="green" />
+                            </div>
+                            <div className="flex items-center">
+                                <ChevronRightIcon className="h-5 w-5 text-green-800 ml-3 mt-2" />
                             </div>
                         </div>
-                        <Progress value={100} size="lg" className="mt-2" color="red" />
-                    </div>
+                    </Link>
+
+                    <Link>
+                        <div className="mb-2 flex items-center">
+                            <div className="flex-1 w-0">
+                                <div className="flex items-center justify-between mb-2">
+                                    <Typography className="text-gray-900 font-medium mt-2 truncate">
+                                        Tobacco
+                                    </Typography>
+                                    <div className="flex gap-4 items-center">
+                                        <Typography className="text-gray-900 font-semibold mt-2">
+                                            -$5,00
+                                        </Typography>
+                                        <Typography className="text-gray-700 font-medium mt-2 text-sm">
+                                            -10%
+                                        </Typography>
+                                    </div>
+                                </div>
+                                <Progress value={100} size="lg" className="mt-2" color="red" />
+                            </div>
+                            <div className="flex items-center">
+                                <ChevronRightIcon className="h-5 w-5 text-green-800 ml-3 mt-2" />
+                            </div>
+                        </div>
+                    </Link>
                 </div>
 
                 <hr className="my-2 border-blue-gray-50 mb-6 mt-6" />
 
                 <div>
-                    <Typography variant="h6" className="text-gray-900">
+                    <Typography variant="h6" className="text-gray-900 mb-2">
                         Monthly
                     </Typography>
-                    <div className="mt-2">
-                        <div className="flex items-center justify-between mb-2">
-                            <Typography className="text-gray-900 font-medium mt-2">
-                                General
-                            </Typography>
-                            <div className="flex gap-4 items-center">
-                                <Typography className="text-gray-900 font-semibold mt-2">
-                                    $235,15
-                                </Typography>
-                                <Typography className="text-gray-700 font-medium mt-2 text-sm">
-                                    10%
-                                </Typography>
-                            </div>
-                        </div>
-                        <Progress value={90} size="lg" className="mt-2" color="orange" />
-                    </div>
 
-                    <div className="mt-2">
-                        <div className="flex items-center justify-between mb-2">
-                            <Typography className="text-gray-900 font-medium mt-2">
-                                Car
-                            </Typography>
-                            <div className="flex gap-4 items-center">
-                                <Typography className="text-gray-900 font-semibold mt-2">
-                                    150,00
-                                </Typography>
-                                <Typography className="text-gray-700 font-medium mt-2 text-sm">
-                                    35%
-                                </Typography>
+                    <Link>
+                        <div className="mb-2 flex items-center">
+                            <div className="flex-1 w-0">
+                                <div className="flex items-center justify-between mb-2">
+                                    <Typography className="text-gray-900 font-medium mt-2 truncate">
+                                        General
+                                    </Typography>
+                                    <div className="flex gap-4 items-center">
+                                        <Typography className="text-gray-900 font-semibold mt-2">
+                                            $235,15
+                                        </Typography>
+                                        <Typography className="text-gray-700 font-medium mt-2 text-sm">
+                                            10%
+                                        </Typography>
+                                    </div>
+                                </div>
+                                <Progress value={90} size="lg" className="mt-2" color="orange" />
+                            </div>
+                            <div className="flex items-center">
+                                <ChevronRightIcon className="h-5 w-5 text-green-800 ml-3 mt-2" />
                             </div>
                         </div>
-                        <Progress value={65} size="lg" className="mt-2" color="green" />
-                    </div>
+                    </Link>
+
+                    <Link>
+                        <div className="mb-2 flex items-center">
+                            <div className="flex-1 w-0">
+                                <div className="flex items-center justify-between mb-2">
+                                    <Typography className="text-gray-900 font-medium mt-2 truncate">
+                                        Car
+                                    </Typography>
+                                    <div className="flex gap-4 items-center">
+                                        <Typography className="text-gray-900 font-semibold mt-2">
+                                            $150,00
+                                        </Typography>
+                                        <Typography className="text-gray-700 font-medium mt-2 text-sm">
+                                            35%
+                                        </Typography>
+                                    </div>
+                                </div>
+                                <Progress value={65} size="lg" className="mt-2" color="green" />
+                            </div>
+                            <div className="flex items-center">
+                                <ChevronRightIcon className="h-5 w-5 text-green-800 ml-3 mt-2" />
+                            </div>
+                        </div>
+                    </Link>
                 </div>
+
+
 
                 <hr className="my-2 border-blue-gray-50 mb-6 mt-6" />
 
