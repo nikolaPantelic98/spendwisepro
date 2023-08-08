bc6a1923a5d46ed5091a71d4ddad61e7c53e1624
nikola <nikolapantelic998@gmail.com>
2023-08-08 14:53:15 +0200

    Income tab new design in the add record page

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/record/AddRecordIncome.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/record/AddRecordIncome.js
@@ -1,150 +1,39 @@
-import {
-    Card,
-    CardBody,
-    Chip, ListItem,
-} from "@material-tailwind/react";
-import {ChevronRightIcon} from "@heroicons/react/24/outline";
-import React from "react";
-import {Link} from "react-router-dom";
+import { Button, Card, CardBody } from "@material-tailwind/react";
+import React, {useState} from "react";
+import AddRecordAmount from "./add-record-elements/AddRecordAmount";
+import AddRecordCategory from "./add-record-elements/AddRecordCategory";
+import AddRecordPaymentType from "./add-record-elements/AddRecordPaymentType";
+import AddRecordDateAndTime from "./add-record-elements/AddRecordDateAndTime";
+import AddRecordNote from "./add-record-elements/AddRecordNote";
+import AddRecordCreditCard from "./add-record-elements/AddRecordCreditCard";
 
-export default function AddRecordExpense() {
+export default function AddRecordIncome() {
 
-    function scrollToTop() {
-        window.scrollTo(0, 0);
-    }
+    const [selectedPaymentType, setSelectedPaymentType] = useState("Cash");
 
     return (
         <Card className="w-full shadow-lg mt-8">
             <CardBody>
                 <div className="flow-root">
                     <ul role="list" className="divide-y divide-gray-200">
+                        <AddRecordAmount />
 
-                        <li className="py-3 sm:py-4">
-                            <Link>
-                                <ListItem className="flex items-center space-x-4 text-left p-0 focus:bg-green-50 hover:bg-green-50">
-                                    <div className="flex-shrink-0">
-                                        <img className="w-8 h-8 rounded-full" src="https://cdn-icons-png.flaticon.com/512/189/189715.png" alt="Electricity bills" />
-                                    </div>
-                                    <div className="flex-1 min-w-0">
-                                        <p className="text-sm font-semibold text-green-700 truncate dark:text-white">
-                                            Amount
-                                        </p>
-                                    </div>
-                                    <div className="text-right">
-                                        <div className="h-4"></div>
-                                        <div className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            Type
-                                        </div>
-                                        <div className="h-4"></div>
-                                    </div>
-                                    <div className="ml-2">
-                                        <ChevronRightIcon className="h-5 w-5 text-gray-900" />
-                                    </div>
-                                </ListItem>
-                            </Link>
-                        </li>
+                        <AddRecordCategory />
 
-                        <li className="py-3 sm:py-4">
-                            <Link>
-                                <ListItem className="flex items-center space-x-4 text-left p-0 focus:bg-green-50 hover:bg-green-50">
-                                    <div className="flex-shrink-0">
-                                        <img className="w-8 h-8 rounded-full" src="https://cdn-icons-png.flaticon.com/512/107/107073.png" alt="Chips" />
-                                    </div>
-                                    <div className="flex-1 min-w-0">
-                                        <p className="text-sm font-semibold text-green-700 truncate dark:text-white">
-                                            Category
-                                        </p>
-                                    </div>
-                                    <div className="text-right">
-                                        <div className="h-4"></div>
-                                        <div className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            Select
-                                        </div>
-                                        <div className="h-4"></div>
-                                    </div>
-                                    <div className="ml-2">
-                                        <ChevronRightIcon className="h-5 w-5 text-gray-900" />
-                                    </div>
-                                </ListItem>
-                            </Link>
-                        </li>
+                        <AddRecordPaymentType onChange={setSelectedPaymentType} />
 
-                        <li className="py-3 sm:py-4">
-                            <Link>
-                                <ListItem className="flex items-center space-x-4 text-left p-0 focus:bg-green-50 hover:bg-green-50">
-                                    <div className="flex-shrink-0">
-                                        <img className="w-8 h-8 rounded-full" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Circle-icons-creditcard.svg/480px-Circle-icons-creditcard.svg.png" alt="Phone bills" />
-                                    </div>
-                                    <div className="flex-1 min-w-0">
-                                        <p className="text-sm font-semibold text-green-700 truncate dark:text-white">
-                                            Payment type
-                                        </p>
-                                    </div>
-                                    <div className="text-right">
-                                        <div className="h-4"></div>
-                                        <div className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            Select
-                                        </div>
-                                        <div className="h-4"></div>
-                                    </div>
-                                    <div className="ml-2">
-                                        <ChevronRightIcon className="h-5 w-5 text-gray-900" />
-                                    </div>
-                                </ListItem>
-                            </Link>
-                        </li>
+                        {selectedPaymentType === "Credit Card" && <AddRecordCreditCard />}
 
-                        <li className="py-3 sm:py-4">
-                            <Link>
-                                <ListItem className="flex items-center space-x-4 text-left p-0 focus:bg-green-50 hover:bg-green-50">
-                                    <div className="flex-shrink-0">
-                                        <img className="w-8 h-8 rounded-full" src="https://cdn1.iconfinder.com/data/icons/ui-5/502/calendar-512.png" alt="Phone bills" />
-                                    </div>
-                                    <div className="flex-1 min-w-0">
-                                        <p className="text-sm font-semibold text-green-700 truncate dark:text-white">
-                                            Date and time
-                                        </p>
-                                    </div>
-                                    <div className="text-right">
-                                        <div className="h-4"></div>
-                                        <div className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            Select
-                                        </div>
-                                        <div className="h-4"></div>
-                                    </div>
-                                    <div className="ml-2">
-                                        <ChevronRightIcon className="h-5 w-5 text-gray-900" />
-                                    </div>
-                                </ListItem>
-                            </Link>
-                        </li>
+                        <AddRecordDateAndTime />
 
-                        <li className="py-3 sm:py-4">
-                            <Link>
-                                <ListItem className="flex items-center space-x-4 text-left p-0 focus:bg-green-50 hover:bg-green-50">
-                                    <div className="flex-shrink-0">
-                                        <img className="w-8 h-8 rounded-full" src="https://cdn-icons-png.flaticon.com/512/190/190703.png" alt="Phone bills" />
-                                    </div>
-                                    <div className="flex-1 min-w-0">
-                                        <p className="text-sm font-semibold text-green-700 truncate dark:text-white">
-                                            Note
-                                        </p>
-                                    </div>
-                                    <div className="text-right">
-                                        <div className="h-4"></div>
-                                        <div className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            Type
-                                        </div>
-                                        <div className="h-4"></div>
-                                    </div>
-                                    <div className="ml-2">
-                                        <ChevronRightIcon className="h-5 w-5 text-gray-900" />
-                                    </div>
-                                </ListItem>
-                            </Link>
-                        </li>
-
+                        <AddRecordNote />
                     </ul>
+                    <hr className="my-2 border-blue-gray-50" />
+                    <div className="flex justify-center items-center">
+                        <Button className="mt-2 w-full" variant="gradient" color="green">
+                            <span>Save</span>
+                        </Button>
+                    </div>
                 </div>
             </CardBody>
         </Card>
