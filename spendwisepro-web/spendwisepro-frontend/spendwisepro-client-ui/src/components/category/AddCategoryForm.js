9c6d8b7cb321c15a24f8b456a91d1b7e11dc0edb
nikola <nikolapantelic998@gmail.com>
2023-08-10 23:15:08 +0200

    Added bar chart for last 7 days expenses

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/category/AddCategoryForm.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/category/AddCategoryForm.js
@@ -10,61 +10,6 @@ import AddCategoryIcon from "./add-category-elements/AddCategoryIcon";
 
 export default function AddCategoryForm() {
     return (
-        // <Card className="mt-6">
-        //     <CardBody>
-        //
-        //         <div className="m-6">
-        //             <Input label="Name" color="green" />
-        //         </div>
-        //
-        //         <div className="m-6">
-        //             <Select label="Parent Category" menuProps={{ className: "h-48" }} color="green">
-        //                 <Option value="None">None</Option>
-        //                 <Option value="Grocery">Grocery</Option>
-        //                 <Option value="Fuel">Fuel</Option>
-        //                 <Option value="Health Care">Health Care</Option>
-        //                 <Option value="Phone">Phone</Option>
-        //                 <Option value="Tax">Tax</Option>
-        //                 <Option value="Sales">Sales</Option>
-        //                 <Option value="Salary">Salary</Option>
-        //             </Select>
-        //             <Typography variant="small" color="gray" className="flex items-center gap-1 font-normal mt-2">
-        //                 <InformationCircleIcon className="w-4 h-4 -mt-px" />
-        //                 Can be empty.
-        //             </Typography>
-        //         </div>
-        //
-        //         <div className="m-6">
-        //             <Button
-        //                 size="sm"
-        //                 variant="outlined"
-        //                 className="flex items-center gap-3 text-green-700 hover:text-green-800"
-        //                 color="green"
-        //                 onClick={() => {
-        //                     document.getElementById("upload-input").click();
-        //                 }}
-        //             >
-        //                 Upload icon
-        //                 <ArrowUpOnSquareIcon strokeWidth={2} className="h-5 w-5" />
-        //             </Button>
-        //             <input
-        //                 id="upload-input"
-        //                 type="file"
-        //                 className="hidden"
-        //                 onChange={(e) => {
-        //                     const fileName = e.target.files[0].name;
-        //                     document.getElementById("file-name").textContent = fileName;
-        //                 }}
-        //             />
-        //             <p id="file-name" className="mt-2"></p>
-        //         </div>
-        //
-        //         <div className="m-6 mt-8">
-        //             <Button size="md" color="green" className="bg-green-700 hover:bg-green-800">Add</Button>
-        //         </div>
-        //
-        //     </CardBody>
-        // </Card>
 
         <Card className="w-full shadow-lg mt-8">
             <CardBody>
--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/expenses/ExpensesOverviewWeek.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/expenses/ExpensesOverviewWeek.js
@@ -2,37 +2,98 @@ import {
     Card,
     CardBody,
     Typography,
-    Chip,
+    Chip
 } from "@material-tailwind/react";
+import {
+    Bar,
+    BarChart,
+    CartesianGrid,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis
+} from "recharts";
 import React from "react";
-import { Progress } from "@material-tailwind/react";
+import {ChartBarIcon} from "@heroicons/react/24/solid";
 
 export default function ExpensesOverviewWeek() {
+
+    const dataWeek = [
+        {"date": "15.7", "amount": 60},
+        {"date": "16.7", "amount": 100},
+        {"date": "17.7", "amount": 20},
+        {"date": "18.7", "amount": 0},
+        {"date": "19.7", "amount": 30},
+        {"date": "20.7", "amount": 0},
+        {"date": "21.7.", "amount": 70}
+    ];
+
+    const TooltipContentWeek = ({ active, payload }) => {
+        if (active && payload && payload.length) {
+            const dataWeek = payload[0];
+            if (dataWeek.payload.amount !== undefined) {
+                return (
+                    <div className="p-1 pl-2 pr-2">
+                        <p className="text-gray-900 border-b-2">{`${dataWeek.payload.date}`}</p>
+                        <p className="font-semibold text-right text-green-chart mt-1 mb-1">{`Spent: $${dataWeek.payload.amount}`}</p>
+                    </div>
+                );
+            }
+        }
+
+        return null;
+    };
 
     return (
         <Card className="w-full shadow-lg mt-8">
             <CardBody>
-                <div className="mt-2">
-                    <div className="flex items-center justify-between mb-2">
-                        <div className="flex-1 w-0">
-                            <Typography  className="uppercase text-sm font-medium text-gray-700">
-                                Last 7 days
-                            </Typography>
-                            <div className="flex items-center justify-between mb-2">
-                                <div className="flex items-center">
-                                    <Typography variant="h4" className="text-gray-900 font-bold mt-2 truncate">
-                                        $270,00
-                                    </Typography>
+                <div>
+                    <Typography variant="h4" color="blue-gray" className="mb-2 flex items-center justify-between">
+                        <span className="mb-2">Overview</span>
+                        <ChartBarIcon className="text-green-700 w-10 h-10 mb-4" />
+                    </Typography>
+                </div>
+
+                <hr className="my-2 border-blue-gray-50 mb-4" />
+
+                <div className="flow-root">
+                    <div className="flow-root">
+                        <div>
+                            <div className="flex-1">
+                                <div className="flex items-center justify-between">
+                                    <p className="text-xs font-medium text-gray-900 truncate">
+                                        LAST 7 DAYS
+                                    </p>
                                 </div>
-                                <div className="flex gap-4 items-center">
-                                    <Chip size="md" value="Total" variant="ghost" className="bg-gray-200 normal-case text-gray-900 font-semibold mt-2 text-sm" />
+                                <div className="flex items-center justify-between">
+                                    <Typography variant="h2" className="text-gray-900 mb-4">
+                                        $280,00
+                                    </Typography>
+                                    <Chip size="md" value="Total" className="bg-gray-200 normal-case text-gray-900 font-semibold mb-4 text-sm" />
                                 </div>
                             </div>
-                            <Progress value={100} size="lg" className="mt-2 mb-2" color="green" />
+
+                            <div>
+                                <ResponsiveContainer width="100%" height={220}>
+                                    <BarChart className="right-4" data={dataWeek} margin={{ top: 20, right: 0, left: 0, bottom: 0 }}>
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="date" />
+                                        <YAxis />
+                                        <Tooltip cursor={{fill: '#E8F5E9'}}
+                                                 payloadArray={dataWeek}
+                                                 content={<TooltipContentWeek />}
+                                                 wrapperStyle={{ background: 'white', border: '2px solid #ddd',  borderRadius: '8px', padding: '5px' }}
+                                                 offset={25}/>
+                                        <Bar dataKey="amount" fill="#82ca9d" />
+                                    </BarChart>
+                                </ResponsiveContainer>
+                            </div>
                         </div>
                     </div>
                 </div>
+
             </CardBody>
+
         </Card>
     );
 }
