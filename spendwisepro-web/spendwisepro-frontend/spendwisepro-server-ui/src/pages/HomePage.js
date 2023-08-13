0311d4d71773b509637706bab609d70d48b5d6a4
nikola <nikolapantelic998@gmail.com>
2023-08-13 20:33:19 +0200

    Home page for admins created

---
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-server-ui/src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function HomePage() {
+
+    return (
+        <>
+            <div className="overflow-hidden">
+
+                <div className="h-6 bg-green-50"></div>
+
+                <div className="flex justify-center min-h-screen bg-green-50">
+                    <div className="mt-2">
+
+                        <div className="mx-6">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default HomePage;
