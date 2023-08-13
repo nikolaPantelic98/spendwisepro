d14d8ef4a5e06c5858afb2d15002f90a409492c0
nikola <nikolapantelic998@gmail.com>
2023-08-13 20:17:07 +0200

    Spendwisepro admin react project for frontend created and configured wihtin the multi-module architecture

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-server-ui/tailwind.config.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-server-ui/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        '1': '1px',
+        '3': '3px',
+      }
+    },
   },
   plugins: [],
-}
+});
