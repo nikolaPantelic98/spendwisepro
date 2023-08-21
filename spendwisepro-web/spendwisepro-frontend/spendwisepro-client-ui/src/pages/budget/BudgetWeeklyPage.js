a959382c56bc557ad6878ecffd88c872c64b400c
nikola <nikolapantelic998@gmail.com>
2023-08-22 00:50:56 +0200

    Bug fixed for not showing the proper header name in the weekly budgets

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/pages/budget/BudgetWeeklyPage.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/pages/budget/BudgetWeeklyPage.js
@@ -2,8 +2,26 @@ import React, { useState } from 'react';
 import Menu from '../../components/common/Menu';
 import BudgetWeeklyTabs from "../../components/budget/BudgetWeeklyTabs";
 import PageHeader from "../../components/common/PageHeader";
+import {useParams} from "react-router-dom";
 
 function BudgetWeeklyPage() {
+    const { '*': name } = useParams();
+
+    // formatting the name of the category from the path
+    const formatName = (name) => {
+        const words = name.split('_');
+        const formattedWords = words.map((word, index) => {
+            if (index === 0) {
+                return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+            } else {
+                return word.toLowerCase();
+            }
+        });
+        return formattedWords.join(' ');
+    };
+
+    const formattedName = formatName(name);
+
     const [sidebarOpen, setSidebarOpen] = useState(false);
 
     const toggleSidebar = (isOpen) => {
@@ -19,7 +37,7 @@ function BudgetWeeklyPage() {
             <div className="h-6 bg-green-50"></div>
 
             <div>
-                <PageHeader title="General" />
+                <PageHeader title={formattedName} />
             </div>
 
             <div className=" flex justify-center min-h-screen bg-green-50">
