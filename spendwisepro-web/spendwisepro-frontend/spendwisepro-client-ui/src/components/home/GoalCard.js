81b1e0f4b8ec1caf0f936a93e3e17b5e5e4a54de
nikola <nikolapantelic998@gmail.com>
2023-09-02 18:29:50 +0200

    Added interface when there is no goal in the goal card on the home page

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/home/GoalCard.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/home/GoalCard.js
@@ -1,5 +1,5 @@
 import {Button, Card, CardBody, ListItem, Progress, Typography,} from "@material-tailwind/react";
-import {ArrowLongRightIcon, ChevronRightIcon} from "@heroicons/react/24/outline";
+import {ArrowLongRightIcon, BanknotesIcon, ChevronRightIcon, CircleStackIcon} from "@heroicons/react/24/outline";
 import React from "react";
 import {Link} from "react-router-dom";
 import moment from 'moment-timezone';
@@ -266,104 +266,147 @@ export default function GoalCard() {
             <CardBody>
                 <Typography variant="h5" color="blue-gray" className="mb-4 flex items-center justify-between">
                     Goals
-                    <Link to="/goals" onClick={storeScrollPosition}>
-                        <Button size="sm" variant="text" className="flex gap-2">
-                                Show more
-                            <ArrowLongRightIcon strokeWidth={2} className="w-4 h-4" />
-                        </Button>
-                    </Link>
+                    {goals.length > 0 && (
+                        <Link to="/goals" onClick={storeScrollPosition}>
+                            <Button size="sm" variant="text" className="flex gap-2">
+                                    Show more
+                                <ArrowLongRightIcon strokeWidth={2} className="w-4 h-4" />
+                            </Button>
+                        </Link>
+                    )}
                 </Typography>
 
                 <hr className="my-2 border-blue-gray-50 mb-6" />
 
-                <div>
-                    <Typography variant="h6" className="text-gray-900 mb-2">
-                        Weekly
-                    </Typography>
+                {goals.length > 0 && (
+                    <div>
+                        <div>
+                            {goals.filter(goal => goal.period === "weekly").length > 0 && (
+                                <Typography variant="h6" className="text-gray-900 mb-2">
+                                    Weekly
+                                </Typography>
+                            )}
 
-                    {weeklyGoals
-                        .map((goal) => (
-                            <div key={goal.id}>
-                                <Link to={`/goals/weekly/${generatePath(goal.name)}`} onClick={storeScrollPosition}>
-                                    <ListItem className="mb-2 flex items-center space-x-4 text-left p-0 focus:bg-green-50 hover:bg-green-50">
-                                        <div className="flex-1 w-0">
-                                            <div className="flex items-center justify-between mb-2">
-                                                <Typography className="text-gray-900 font-medium mt-2 truncate">
-                                                    {goal.name}
-                                                </Typography>
-                                                <div className="flex gap-4 items-center">
-                                                    <Typography className="text-gray-900 font-semibold mt-2">
-                                                        {goal.remainingAmountToBeSaved.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2, maximumFractionDigits: 2 })}
-                                                    </Typography>
-                                                    <Typography className="text-gray-700 font-medium mt-2 text-sm">
-                                                        {calculateSavedPercentage(goal.amount, goal.savedAmount).toFixed(0)}%
-                                                    </Typography>
-                                                </div>
-                                            </div>
-                                            <Progress value={calculateSavedPercentage(goal.amount, goal.savedAmount)} size="lg" className="mt-2 mb-2" color={generateProgressColor(goal.amount, goal.savedAmount)} />
+                            {weeklyGoals
+                                .map((goal) => (
+                                        <div key={goal.id}>
+                                            <Link to={`/goals/weekly/${generatePath(goal.name)}`} onClick={storeScrollPosition}>
+                                                <ListItem className="mb-2 flex items-center space-x-4 text-left p-0 focus:bg-green-50 hover:bg-green-50">
+                                                    <div className="flex-1 w-0">
+                                                        <div className="flex items-center justify-between mb-2">
+                                                            <Typography className="text-gray-900 font-medium mt-2 truncate">
+                                                                {goal.name}
+                                                            </Typography>
+                                                            <div className="flex gap-4 items-center">
+                                                                <Typography className="text-gray-900 font-semibold mt-2">
+                                                                    {goal.remainingAmountToBeSaved.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                                                                </Typography>
+                                                                <Typography className="text-gray-700 font-medium mt-2 text-sm">
+                                                                    {calculateSavedPercentage(goal.amount, goal.savedAmount).toFixed(0)}%
+                                                                </Typography>
+                                                            </div>
+                                                        </div>
+                                                        <Progress value={calculateSavedPercentage(goal.amount, goal.savedAmount)} size="lg" className="mt-2 mb-2" color={generateProgressColor(goal.amount, goal.savedAmount)} />
+                                                    </div>
+                                                    <div className="flex items-center">
+                                                        <ChevronRightIcon className="h-5 w-5 text-green-800 mt-2 mb-2" />
+                                                    </div>
+                                                </ListItem>
+                                            </Link>
                                         </div>
-                                        <div className="flex items-center">
-                                            <ChevronRightIcon className="h-5 w-5 text-green-800 mt-2 mb-2" />
+                                    )
+                                )}
+                        </div>
+
+                        {goals.filter(goal => goal.period === "weekly").length > 0 && (
+                            <hr className="my-2 border-blue-gray-50 mb-6 mt-6" />
+                        )}
+
+                        <div>
+                            {goals.filter(goal => goal.period === "monthly").length > 0 && (
+                                <Typography variant="h6" className="text-gray-900 mb-2">
+                                    Monthly
+                                </Typography>
+                            )}
+
+                            {monthlyGoals
+                                .map((goal) => (
+                                        <div key={goal.id}>
+                                            <Link to={`/goals/monthly/${generatePath(goal.name)}`} onClick={storeScrollPosition}>
+                                                <ListItem className="mb-2 flex items-center space-x-4 text-left p-0 focus:bg-green-50 hover:bg-green-50">
+                                                    <div className="flex-1 w-0">
+                                                        <div className="flex items-center justify-between mb-2">
+                                                            <Typography className="text-gray-900 font-medium mt-2 truncate">
+                                                                {goal.name}
+                                                            </Typography>
+                                                            <div className="flex gap-4 items-center">
+                                                                <Typography className="text-gray-900 font-semibold mt-2">
+                                                                    {goal.remainingAmountToBeSaved.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                                                                </Typography>
+                                                                <Typography className="text-gray-700 font-medium mt-2 text-sm">
+                                                                    {calculateSavedPercentage(goal.amount, goal.savedAmount).toFixed(0)}%
+                                                                </Typography>
+                                                            </div>
+                                                        </div>
+                                                        <Progress value={calculateSavedPercentage(goal.amount, goal.savedAmount)} size="lg" className="mt-2 mb-2" color={generateProgressColor(goal.amount, goal.savedAmount)} />
+                                                    </div>
+                                                    <div className="flex items-center">
+                                                        <ChevronRightIcon className="h-5 w-5 text-green-800 mt-2 mb-2" />
+                                                    </div>
+                                                </ListItem>
+                                            </Link>
                                         </div>
-                                    </ListItem>
-                                </Link>
-                            </div>
-                        )
-                    )}
-                </div>
+                                    )
+                                )}
+                        </div>
 
-                <hr className="my-2 border-blue-gray-50 mb-6 mt-6" />
+                        {goals.filter(goal => goal.period === "monthly").length > 0 && (
+                            <hr className="my-2 border-blue-gray-50 mb-6 mt-6" />
+                        )}
 
-                <div>
-                    <Typography variant="h6" className="text-gray-900 mb-2">
-                        Monthly
-                    </Typography>
+                        <div className="container mx-auto mt-6">
+                            <p className="text-xs flex items-center justify-center gap-4">
+                            <span className="flex items-center">
+                                <span className="w-3 h-3 inline-block mr-1 bg-green-500"></span>
+                                <span className="text-xxs">Completed</span>
+                            </span>
+                                <span className="flex items-center">
+                                <span className="w-3 h-3 inline-block mr-1 bg-orange-500"></span>
+                                <span className="text-xxs">In progress</span>
+                            </span>
+                            </p>
+                        </div>
+                    </div>
+                )}
 
-                    {monthlyGoals
-                        .map((goal) => (
-                            <div key={goal.id}>
-                                <Link to={`/goals/monthly/${generatePath(goal.name)}`} onClick={storeScrollPosition}>
-                                    <ListItem className="mb-2 flex items-center space-x-4 text-left p-0 focus:bg-green-50 hover:bg-green-50">
-                                        <div className="flex-1 w-0">
-                                            <div className="flex items-center justify-between mb-2">
-                                                <Typography className="text-gray-900 font-medium mt-2 truncate">
-                                                    {goal.name}
-                                                </Typography>
-                                                <div className="flex gap-4 items-center">
-                                                    <Typography className="text-gray-900 font-semibold mt-2">
-                                                        {goal.remainingAmountToBeSaved.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2, maximumFractionDigits: 2 })}
-                                                    </Typography>
-                                                    <Typography className="text-gray-700 font-medium mt-2 text-sm">
-                                                        {calculateSavedPercentage(goal.amount, goal.savedAmount).toFixed(0)}%
-                                                    </Typography>
-                                                </div>
-                                            </div>
-                                            <Progress value={calculateSavedPercentage(goal.amount, goal.savedAmount)} size="lg" className="mt-2 mb-2" color={generateProgressColor(goal.amount, goal.savedAmount)} />
-                                        </div>
-                                        <div className="flex items-center">
-                                            <ChevronRightIcon className="h-5 w-5 text-green-800 mt-2 mb-2" />
-                                        </div>
-                                    </ListItem>
-                                </Link>
-                            </div>
-                        )
-                    )}
-                </div>
-
-                <hr className="my-2 border-blue-gray-50 mb-6 mt-6" />
-
-                <div className="container mx-auto mt-6">
-                    <p className="text-xs flex items-center justify-center gap-4">
-                        <span className="flex items-center">
-                            <span className="w-3 h-3 inline-block mr-1 bg-green-500"></span>
-                            <span className="text-xxs">Completed</span>
-                        </span>
-                        <span className="flex items-center">
-                            <span className="w-3 h-3 inline-block mr-1 bg-orange-500"></span>
-                            <span className="text-xxs">In progress</span>
-                        </span>
-                    </p>
-                </div>
+                {goals.length === 0 && (
+                    <div>
+                        <div className="flex justify-center items-center flex-col mb-6">
+                            <CircleStackIcon className="w-20 h-20 text-green-600 mb-2"/>
+                            <Typography variant="h6" className="text-gray-600">
+                                You don't have
+                            </Typography>
+                            <Typography variant="h6" className="text-gray-600">
+                                active goals
+                            </Typography>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <Link to="/add_goal" onClick={storeScrollPosition} className="w-full">
+                                <Button className="w-full" variant="gradient" color="green">
+                                    <span>Add weekly goal</span>
+                                </Button>
+                            </Link>
+                        </div>
+                        <div className="h-2"></div>
+                        <div className="flex justify-center items-center">
+                            <Link to="/add_goal" onClick={storeScrollPosition} className="w-full">
+                                <Button className="w-full" variant="gradient" color="green">
+                                    <span>Add monthly goal</span>
+                                </Button>
+                            </Link>
+                        </div>
+                    </div>
+                )}
 
                 <div className="h-6"></div>
             </CardBody>
