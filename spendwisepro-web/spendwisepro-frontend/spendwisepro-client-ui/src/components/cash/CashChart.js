ad74d2cd0f44decba45dcb67cbbc4a577a3d3232
nikola <nikolapantelic998@gmail.com>
2023-09-07 14:00:10 +0200

    Created cash trend chart for the last 30 days

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/cash/CashChart.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/cash/CashChart.js
@@ -61,6 +61,305 @@ export default function CashChart() {
 
     const [selectedTab, setSelectedTab] = useState("30days");
 
+    const records = [
+        {
+            id: 1,
+            amount: 30.00,
+            type: "expense",
+            date: new Date("2023-08-16T08:57"),
+            note: "Window repair",
+            paymentType: "Credit Card",
+            category: [
+                { id: 1, categoryName: "House and garden", icon: "https://i.ibb.co/P6tb18T/house.png" }
+            ]
+        },
+        {
+            id: 2,
+            amount: 25.00,
+            type: "expense",
+            date: new Date("2023-08-20T12:30"),
+            note: "New door",
+            paymentType: "Cash",
+            category: [
+                { id: 1, categoryName: "House and garden", icon: "https://i.ibb.co/P6tb18T/house.png" }
+            ]
+        },
+        {
+            id: 3,
+            amount: 45.00,
+            type: "expense",
+            date: new Date("2023-08-16T12:30"),
+            note: "Garden maintenance",
+            paymentType: "Credit Card",
+            category: [
+                { id: 1, categoryName: "House and garden", icon: "https://i.ibb.co/P6tb18T/house.png" }
+            ]
+        },
+        {
+            id: 4,
+            amount: 45.00,
+            type: "expense",
+            date: new Date("2023-08-30T12:30"),
+            note: "Tomato",
+            paymentType: "Credit Card",
+            category: [
+                { id: 2, categoryName: "Groceries", icon: "https://icon-library.com/images/grocery-icon-png/grocery-icon-png-14.jpg" }
+            ]
+        },
+        {
+            id: 5,
+            amount: 100.00,
+            type: "expense",
+            date: new Date("2023-08-10T08:57"),
+            note: "Car maintenance",
+            paymentType: "Cash",
+            category: [
+                { id: 3, categoryName: "Car", icon: "https://i.ibb.co/p0Sc2Bs/car.png" }
+            ]
+        },
+        {
+            id: 6,
+            amount: 112.00,
+            type: "expense",
+            date: new Date("2023-08-21T12:30"),
+            note: "Broken window repair",
+            paymentType: "Credit Card",
+            category: [
+                { id: 3, categoryName: "Car", icon: "https://i.ibb.co/p0Sc2Bs/car.png" }
+            ]
+        },
+        {
+            id: 7,
+            amount: 45.00,
+            type: "expense",
+            date: new Date("2023-09-01T12:30"),
+            note: "Fuel",
+            paymentType: "Credit Card",
+            category: [
+                { id: 3, categoryName: "Car", icon: "https://i.ibb.co/p0Sc2Bs/car.png" }
+            ]
+        },
+        {
+            id: 8,
+            amount: 15.00,
+            type: "expense",
+            date: new Date("2023-08-21T08:57"),
+            note: "Cigarette",
+            paymentType: "Cash",
+            category: [
+                { id: 4, categoryName: "Tobacco", icon: "https://www.iconbunny.com/icons/media/catalog/product/3/0/3050.12-cigarette-icon-iconbunny.jpg" }
+            ]
+        },
+        {
+            id: 91,
+            amount: 27.00,
+            type: "expense",
+            date: new Date("2023-08-18T12:30"),
+            note: "Tobacco",
+            paymentType: "Credit Card",
+            category: [
+                { id: 4, categoryName: "Tobacco", icon: "https://www.iconbunny.com/icons/media/catalog/product/3/0/3050.12-cigarette-icon-iconbunny.jpg" }
+            ]
+        },
+        {
+            id: 10,
+            amount: 22.00,
+            type: "expense",
+            date: new Date("2023-08-17T12:30"),
+            note: "Pack",
+            paymentType: "Credit Card",
+            category: [
+                { id: 4, categoryName: "Tobacco", icon: "https://www.iconbunny.com/icons/media/catalog/product/3/0/3050.12-cigarette-icon-iconbunny.jpg" }
+            ]
+        },
+        {
+            id: 11,
+            amount: 27.00,
+            type: "expense",
+            date: new Date("2023-08-14T12:30"),
+            time: "12:30",
+            note: "Chips",
+            paymentType: "Credit Card",
+            category: [
+                { id: 5, categoryName: "Snacks", icon: "https://www.iconbunny.com/icons/media/catalog/product/cache/2/thumbnail/600x/1b89f2fc96fc819c2a7e15c7e545e8a9/5/2/524.9-fries-icon-iconbunny.jpg" }
+            ]
+        },
+        {
+            id: 12,
+            amount: 15.00,
+            type: "expense",
+            date: new Date("2023-08-22T08:57"),
+            note: "Doctor",
+            paymentType: "Cash",
+            category: [
+                { id: 6, categoryName: "Health care", icon: "https://i.ibb.co/k362Qsn/healthcare.png" }
+            ]
+        },
+        {
+            id: 13,
+            amount: 15.00,
+            type: "expense",
+            date: new Date("2023-09-01T08:57"),
+            note: "Card",
+            paymentType: "Cash",
+            category: [
+                { id: 7, categoryName: "Cinema", icon: "https://i.ibb.co/GC7MSvb/clapperboard.png" }
+            ]
+        },
+        {
+            id: 14,
+            amount: 27.00,
+            type: "expense",
+            date: new Date("2023-08-23T12:30"),
+            note: "Cinema chips",
+            paymentType: "Credit Card",
+            category: [
+                { id: 7, categoryName: "Cinema", icon: "https://i.ibb.co/GC7MSvb/clapperboard.png" }
+            ]
+        },
+        {
+            id: 15,
+            amount: 600.00,
+            type: "income",
+            date: new Date("2023-08-01T12:30"),
+            note: "Salary",
+            paymentType: "Credit Card",
+            category: [
+                { id: 7, categoryName: "Income", icon: "https://i.ibb.co/GC7MSvb/clapperboard.png" }
+            ]
+        },
+        {
+            id: 16,
+            amount: 400.00,
+            type: "income",
+            date: new Date("2023-08-01T12:30"),
+            note: "Salary",
+            paymentType: "Cash",
+            category: [
+                { id: 7, categoryName: "Income", icon: "https://i.ibb.co/GC7MSvb/clapperboard.png" }
+            ]
+        },
+        {
+            id: 17,
+            amount: 300.00,
+            type: "income",
+            date: new Date("2023-08-28T12:30"),
+            note: "Invoice",
+            paymentType: "Credit Card",
+            category: [
+                { id: 7, categoryName: "Income", icon: "https://i.ibb.co/GC7MSvb/clapperboard.png" }
+            ]
+        },
+        {
+            id: 18,
+            amount: 100.00,
+            type: "expense",
+            date: new Date("2023-09-06T08:57"),
+            note: "Card",
+            paymentType: "Cash",
+            category: [
+                { id: 7, categoryName: "Cinema", icon: "https://i.ibb.co/GC7MSvb/clapperboard.png" }
+            ]
+        }
+    ];
+
+    const currentDate = new Date();
+
+    const thirtyDaysAgo = (() => {
+        const result = new Date(currentDate);
+        result.setDate(currentDate.getDate() - 30);
+        result.setHours(0, 0, 0, 0);
+        return result;
+    })();
+
+    // Filter cash expenses before the last 30 days
+    const cashExpensesBeforeLast30Days = (() => {
+        return records.filter(record => {
+            return record.date < thirtyDaysAgo && record.type === "expense" && record.paymentType === "Cash";
+        });
+    })();
+
+    // Filter cash incomes before the last 30 days
+    const cashIncomesBeforeLast30Days = (() => {
+        return records.filter(record => {
+            return record.date < thirtyDaysAgo && record.type === "income" && record.paymentType === "Cash";
+        });
+    })();
+
+    // Calculate the initial cash amount for the chart
+    const startingAmountCash30Days = (() => {
+        const totalCashExpenseAmountBeforeLast30Days = cashExpensesBeforeLast30Days.reduce((total, record) => total + record.amount, 0);
+        const totalCashIncomeAmountBeforeLast30Days = cashIncomesBeforeLast30Days.reduce((total, record) => total + record.amount, 0);
+
+        return totalCashIncomeAmountBeforeLast30Days - totalCashExpenseAmountBeforeLast30Days;
+    })();
+
+    // Filter cash expenses within the last 30 days
+    const cashExpensesLast30Days = (() => {
+        return records.filter(record => {
+            return record.date >= thirtyDaysAgo && record.type === "expense" && record.paymentType === "Cash";
+        });
+    })();
+
+    // Filter cash incomes within the last 30 days
+    const cashIncomesLast30Days = (() => {
+        return records.filter(record => {
+            return record.date >= thirtyDaysAgo && record.type === "income" && record.paymentType === "Cash";
+        });
+    })();
+
+    // Calculate cash chart data
+    const cashGraphLast30Days = (() => {
+
+        const amountPerDay = [];
+        let accumulatedAmount = startingAmountCash30Days;
+        let iterationDate = new Date(thirtyDaysAgo);
+
+        // Iterate through each day of the last 30 days
+        while (iterationDate <= currentDate) {
+            const matchingExpensesThisDay = cashExpensesLast30Days.filter(record =>
+                record.date.getDate() === iterationDate.getDate() &&
+                record.date.getMonth() === iterationDate.getMonth() &&
+                record.date.getFullYear() === iterationDate.getFullYear()
+            );
+            const matchingIncomesThisDay = cashIncomesLast30Days.filter(record =>
+                record.date.getDate() === iterationDate.getDate() &&
+                record.date.getMonth() === iterationDate.getMonth() &&
+                record.date.getFullYear() === iterationDate.getFullYear()
+            );
+
+            const expensesThisDay = matchingExpensesThisDay.reduce((total, record) => total - record.amount, 0);
+            accumulatedAmount += expensesThisDay;
+            const incomesThisDay = matchingIncomesThisDay.reduce((total, record) => total + record.amount, 0);
+            accumulatedAmount += incomesThisDay;
+
+            amountPerDay.push({
+                date: new Date(iterationDate),
+                amount: accumulatedAmount
+            });
+
+            // Move to the next day
+            iterationDate.setDate(iterationDate.getDate() + 1);
+        }
+
+        return amountPerDay;
+    })();
+
+    // Get the cash amount for today
+    const cashAmountToday = cashGraphLast30Days[cashGraphLast30Days.length - 1].amount;
+
+    const chipAmount30Days = (() => {
+        return (cashAmountToday - startingAmountCash30Days) / startingAmountCash30Days * 100;
+    })();
+
+    const chipColor30Days = (() => {
+        if (chipAmount30Days >= 0) {
+            return "green";
+        } else {
+            return "red";
+        }
+    })();
+
     const TooltipContent = ({ active, payload }) => {
         if (active && payload && payload.length) {
             const data = payload[0];
@@ -126,15 +425,15 @@ export default function CashChart() {
                                                         </div>
                                                         <div className="flex items-center justify-between">
                                                             <Typography variant="h2" className="text-gray-900 mb-4">
-                                                                $700,00
+                                                                {cashAmountToday.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2, maximumFractionDigits: 2 })}
                                                             </Typography>
-                                                            <Chip size="md" value="+ 600%" className="bg-green-700 mb-4 text-sm" />
+                                                            <Chip size="md" value={`${(chipAmount30Days).toFixed(0)}%`} className="mb-4 text-sm" color={chipColor30Days} />
                                                         </div>
                                                     </div>
 
                                                     <div>
                                                         <ResponsiveContainer width="100%" height={220}>
-                                                            <AreaChart className="right-4" data={dataMonth}
+                                                            <AreaChart className="right-4" data={cashGraphLast30Days}
                                                                        margin={{ top: 20, right: 0, left: 0, bottom: 0 }}>
                                                                 <defs>
                                                                     <linearGradient id="chartGreen" x1="0" y1="0" x2="0" y2="1">
@@ -152,7 +451,7 @@ export default function CashChart() {
                                                                 <YAxis tick={{fontSize: 15, dx: -3}} />
                                                                 <CartesianGrid strokeDasharray="3 3" vertical={false}/>
                                                                 <Tooltip cursor={{fill: '#E8F5E9'}}
-                                                                         payloadArray={dataMonth}
+                                                                         payloadArray={cashGraphLast30Days}
                                                                          content={<TooltipContent />}
                                                                          wrapperStyle={{ background: 'white', border: '2px solid #ddd',  borderRadius: '8px', padding: '5px' }}
                                                                          offset={25}/>
