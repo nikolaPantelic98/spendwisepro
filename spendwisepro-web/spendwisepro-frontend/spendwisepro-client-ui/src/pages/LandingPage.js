f26c514a7c1709c894e1f359a708cea854c2d198
nikola <nikolapantelic998@gmail.com>
2023-10-02 21:44:33 +0200

    Menu implemented to the Login and Register page

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/pages/LandingPage.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/pages/LandingPage.js
@@ -4,7 +4,6 @@ import {
     BanknotesIcon, BellIcon, CalendarDaysIcon,
     ChartBarIcon,
     CircleStackIcon,
-    ClipboardDocumentIcon,
     LightBulbIcon
 } from "@heroicons/react/24/outline";
 import MenuLanding from "../components/landing-page/MenuLanding";
--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/pages/login/LoginPage.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/pages/login/LoginPage.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageWidthLayout from "../../components/common/PageWidthLayout";
 import LoginTabs from "../../components/login/LoginTabs";
+import MenuLanding from "../../components/landing-page/MenuLanding";
 
 function LoginPage() {
+
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    const toggleSidebar = (isOpen) => {
+        setSidebarOpen(isOpen);
+    };
 
     return (
         <>
             <div className="overflow-hidden">
+                <MenuLanding sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
                 <div className="h-6 bg-green-50"></div>
 
                 <div className="flex justify-center min-h-screen bg-green-50">
