3742e6771cfb1d3d9d8d349de2caab8bc58426e0
nikola <nikolapantelic998@gmail.com>
2023-10-26 15:52:11 +0200

    Print hierarchical categories unit test created

--- spendwisepro-web/spendwisepro-backend/spendwisepro-client/src/test/java/com/spendwisepro/client/category/CategoryRepositoryTests.java
+++ spendwisepro-web/spendwisepro-backend/spendwisepro-client/src/test/java/com/spendwisepro/client/category/CategoryRepositoryTests.java
@@ -72,4 +72,37 @@ public class CategoryRepositoryTests {
 
         assertThat(children.size()).isGreaterThan(0);
     }
+
+    @Test
+    public void testPrintHierarchicalCategories() {
+        Iterable<Category> categories = categoryRepository.findAll();
+
+        for (Category category : categories) {
+            if (category.getParent() == null) {
+                System.out.println(category.getName());
+
+                Set<Category> children = category.getChildren();
+
+                for (Category subCategory : children) {
+                    System.out.println("--" + subCategory.getName());
+                    printChildren(subCategory, 1);
+                }
+            }
+        }
+    }
+
+    private void printChildren(Category parent, int subLevel) {
+        int newSubLevel = subLevel + 1;
+        Set<Category> children = parent.getChildren();
+
+        for (Category subCategory : children) {
+            for (int i = 0; i < newSubLevel; i++) {
+                System.out.print("--");
+            }
+
+            System.out.println(subCategory.getName());
+
+            printChildren(subCategory, newSubLevel);
+        }
+    }
 }
