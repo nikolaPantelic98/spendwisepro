eb4f6e4e7afa66f443fc34246465907cbc6f3041
nikola <nikolapantelic998@gmail.com>
2023-10-28 15:03:11 +0200

    Icon connected to the category while saving the category on the frontend

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/category/add-category-elements/AddCategoryIcon.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/category/add-category-elements/AddCategoryIcon.js
@@ -9,31 +9,15 @@ import {
     Select,
 } from "@material-tailwind/react";
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
-import React from "react";
-
-const iconOptions = [
-    "https://www.iconbunny.com/icons/media/catalog/product/1/2/1258.9-car-icon-iconbunny.jpg",
-    "https://www.iconbunny.com/icons/media/catalog/product/1/2/1248.9-petrol-pump-icon-iconbunny.jpg",
-    "https://www.iconbunny.com/icons/media/catalog/product/1/2/1228.9-parking-sign-icon-iconbunny.jpg",
-    "https://icon-library.com/images/icon-food/icon-food-13.jpg",
-    "https://icon-library.com/images/grocery-icon-png/grocery-icon-png-14.jpg",
-    "https://cdn-icons-png.flaticon.com/512/227/227326.png",
-    "https://www.iconbunny.com/icons/media/catalog/product/cache/2/thumbnail/600x/1b89f2fc96fc819c2a7e15c7e545e8a9/5/2/524.9-fries-icon-iconbunny.jpg",
-    "https://w7.pngwing.com/pngs/50/319/png-transparent-coffee-icon100-computer-icons-android-pub-orange-logo-coffee-thumbnail.png",
-    "https://cdn-icons-png.flaticon.com/512/186/186056.png",
-    "https://cdn.icon-icons.com/icons2/70/PNG/512/phone_14179.png",
-    "https://www.iconbunny.com/icons/media/catalog/product/1/9/195.9-wifi-icon-iconbunny.jpg",
-    "https://cdn3.iconfinder.com/data/icons/flat-rounded-5/50/482-512.png",
-    "https://cdn2.iconfinder.com/data/icons/online-shopping-flat-round/550/cart-512.png",
-    "https://cdn-icons-png.flaticon.com/512/189/189569.png",
-    "https://www.shareicon.net/data/2016/08/18/809300_diamond_512x512.png",
-    "https://cdn-icons-png.flaticon.com/512/3693/3693434.png",
-];
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function AddCategoryIcon({ onIconChange }) {
     const [openIcon, setOpenIcon] = React.useState(false);
     const [selectedIcon, setSelectedIcon] = React.useState("");
     const [tempSelectedIcon, setTempSelectedIcon] = React.useState("");
+    const [icons, setIcons] = useState([]);
+
     const handleOpenIcon = () => {
         setTempSelectedIcon(selectedIcon);
         setOpenIcon(true);
@@ -44,13 +28,31 @@ export default function AddCategoryIcon({ onIconChange }) {
     };
     const handleConfirmIcon = () => {
         setOpenIcon(false);
-        setSelectedIcon(tempSelectedIcon);
         onIconChange(selectedIcon);
+        setSelectedIcon(tempSelectedIcon);
     };
     const handleIconChange = (value) => {
         setSelectedIcon(value);
         setTempSelectedIcon(value);
     };
+
+    const token = localStorage.getItem("token");
+
+    const headers = {
+        'Authorization': `Bearer ${token}`
+    };
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/spendwisepro/icons/all', { headers })
+            .then(response => {
+                setIcons(response.data);
+            })
+            .catch(error => console.error('Error fetching icons:', error));
+    }, []);
+
+    function findIconImage(icon) {
+        return icon.iconPath;
+    }
 
     return (
         <li className="py-3 sm:py-4">
@@ -80,7 +82,7 @@ export default function AddCategoryIcon({ onIconChange }) {
                             {selectedIcon ? (
                                 <img
                                     className="w-8 h-8 inline-block"
-                                    src={selectedIcon}
+                                    src={findIconImage(selectedIcon)}
                                     alt="Selected Icon"
                                 />
                             ) : (
@@ -113,12 +115,12 @@ export default function AddCategoryIcon({ onIconChange }) {
                         onChange={handleIconChange}
                         className="relative"
                     >
-                        {iconOptions.map((option) => (
-                            <Option value={option} key={option}>
+                        {icons.map((icon) => (
+                            <Option value={icon} key={icon.id}>
                                 <div className="flex items-center justify-center">
                                     <img
                                         className="w-8 h-8 rounded-full"
-                                        src={option}
+                                        src={icon.iconPath}
                                     />
                                 </div>
                             </Option>
