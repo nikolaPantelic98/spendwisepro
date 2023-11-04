bb8e53cd570fb66ce75bedc2a3f94313ca7bd921
nikola <nikolapantelic998@gmail.com>
2023-11-04 13:58:24 +0100

    Credit card icon repository created on the client side

---
+++ spendwisepro-web/spendwisepro-backend/spendwisepro-client/src/main/java/com/spendwisepro/client/creditcardicon/CreditCardIconRepository.java
@@ -0,0 +1,7 @@
+package com.spendwisepro.client.creditcardicon;
+
+import com.spendwisepro.common.entity.CreditCardIcon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreditCardIconRepository extends JpaRepository<CreditCardIcon, Long> {
+}
