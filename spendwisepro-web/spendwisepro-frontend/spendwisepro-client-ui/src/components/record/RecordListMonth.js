26569c58155b482a05685bac1cd39cbe6ff97600
nikola <nikolapantelic998@gmail.com>
2023-11-07 21:36:43 +0100

    View records in the last 30 days functionality implemented

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/record/RecordListMonth.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/record/RecordListMonth.js
@@ -5,182 +5,32 @@ import {
     ListItem,
 } from "@material-tailwind/react";
 import {ChevronRightIcon} from "@heroicons/react/24/outline";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 export default function RecordListMonth() {
 
-    const records = [
-        {
-            id: 1,
-            amount: 30.00,
-            type: "expense",
-            date: new Date("2023-08-16T08:57"),
-            note: "Window repair",
-            paymentType: "Credit Card",
-            category: [
-                { id: 1, categoryName: "House and garden", icon: "https://i.ibb.co/P6tb18T/house.png" }
-            ]
-        },
-        {
-            id: 2,
-            amount: 25.00,
-            type: "expense",
-            date: new Date("2023-08-20T12:30"),
-            note: "New door",
-            paymentType: "Cash",
-            category: [
-                { id: 1, categoryName: "House and garden", icon: "https://i.ibb.co/P6tb18T/house.png" }
-            ]
-        },
-        {
-            id: 3,
-            amount: 45.00,
-            type: "expense",
-            date: new Date("2023-07-29T12:30"),
-            note: "Garden maintenance",
-            paymentType: "Credit Card",
-            category: [
-                { id: 1, categoryName: "House and garden", icon: "https://i.ibb.co/P6tb18T/house.png" }
-            ]
-        },
-        {
-            id: 4,
-            amount: 45.00,
-            type: "expense",
-            date: new Date("2023-07-30T12:30"),
-            note: "Tomato",
-            paymentType: "Credit Card",
-            category: [
-                { id: 2, categoryName: "Groceries", icon: "https://icon-library.com/images/grocery-icon-png/grocery-icon-png-14.jpg" }
-            ]
-        },
-        {
-            id: 5,
-            amount: 100.00,
-            type: "expense",
-            date: new Date("2023-08-10T08:57"),
-            note: "Car maintenance",
-            paymentType: "Cash",
-            category: [
-                { id: 3, categoryName: "Car", icon: "https://i.ibb.co/p0Sc2Bs/car.png" }
-            ]
-        },
-        {
-            id: 6,
-            amount: 112.00,
-            type: "expense",
-            date: new Date("2023-08-21T12:30"),
-            note: "Broken window repair",
-            paymentType: "Credit Card",
-            category: [
-                { id: 3, categoryName: "Car", icon: "https://i.ibb.co/p0Sc2Bs/car.png" }
-            ]
-        },
-        {
-            id: 7,
-            amount: 45.00,
-            type: "expense",
-            date: new Date("2023-08-01T12:30"),
-            note: "Fuel",
-            paymentType: "Credit Card",
-            category: [
-                { id: 3, categoryName: "Car", icon: "https://i.ibb.co/p0Sc2Bs/car.png" }
-            ]
-        },
-        {
-            id: 8,
-            amount: 15.00,
-            type: "expense",
-            date: new Date("2023-08-21T08:57"),
-            note: "Cigarette",
-            paymentType: "Cash",
-            category: [
-                { id: 4, categoryName: "Tobacco", icon: "https://www.iconbunny.com/icons/media/catalog/product/3/0/3050.12-cigarette-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 91,
-            amount: 27.00,
-            type: "expense",
-            date: new Date("2023-08-18T12:30"),
-            note: "Tobacco",
-            paymentType: "Credit Card",
-            category: [
-                { id: 4, categoryName: "Tobacco", icon: "https://www.iconbunny.com/icons/media/catalog/product/3/0/3050.12-cigarette-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 10,
-            amount: 22.00,
-            type: "expense",
-            date: new Date("2023-08-17T12:30"),
-            note: "Pack",
-            paymentType: "Credit Card",
-            category: [
-                { id: 4, categoryName: "Tobacco", icon: "https://www.iconbunny.com/icons/media/catalog/product/3/0/3050.12-cigarette-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 11,
-            amount: 27.00,
-            type: "expense",
-            date: new Date("2023-08-14T12:30"),
-            time: "12:30",
-            note: "Chips",
-            paymentType: "Credit Card",
-            category: [
-                { id: 5, categoryName: "Snacks", icon: "https://www.iconbunny.com/icons/media/catalog/product/cache/2/thumbnail/600x/1b89f2fc96fc819c2a7e15c7e545e8a9/5/2/524.9-fries-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 12,
-            amount: 15.00,
-            type: "expense",
-            date: new Date("2023-08-22T08:57"),
-            note: "Doctor",
-            paymentType: "Cash",
-            category: [
-                { id: 6, categoryName: "Health care", icon: "https://i.ibb.co/k362Qsn/healthcare.png" }
-            ]
-        },
-        {
-            id: 13,
-            amount: 15.00,
-            type: "expense",
-            date: new Date("2023-08-01T08:57"),
-            note: "Card",
-            paymentType: "Cash",
-            category: [
-                { id: 7, categoryName: "Cinema", icon: "https://i.ibb.co/GC7MSvb/clapperboard.png" }
-            ]
-        },
-        {
-            id: 14,
-            amount: 27.00,
-            type: "expense",
-            date: new Date("2023-07-23T12:30"),
-            note: "Cinema chips",
-            paymentType: "Credit Card",
-            category: [
-                { id: 7, categoryName: "Cinema", icon: "https://i.ibb.co/GC7MSvb/clapperboard.png" }
-            ]
-        }
-    ];
+    const [records, setRecords] = useState([]);
 
-    const currentDate = new Date();
+    const token = localStorage.getItem("token");
 
-    // Filter records that occurred in the last 30 days
-    const recordsLastMonth = records.filter(record => {
-        const daysDifference = Math.floor((currentDate - record.date) / (1000 * 60 * 60 * 24));
-        return daysDifference < 30;
-    });
+    const headers = {
+        'Authorization': `Bearer ${token}`
+    };
 
-    recordsLastMonth.sort((a, b) => b.date - a.date);
+    useEffect(() => {
+        axios.get('http://localhost:8000/spendwisepro/records/last_30_days', { headers })
+            .then(response => {
+                setRecords(response.data);
+            })
+            .catch(error => console.error('Error fetching records:', error));
+    }, []);
 
     // Extracts dates without hours and minutes
-    const datesWithoutTime = recordsLastMonth.map(record => {
-        const dateWithoutTime = new Date(record.date);
+    const datesWithoutTime = records.map(record => {
+        let date = new Date(record.dateAndTime);
+        const dateWithoutTime = new Date(date);
         dateWithoutTime.setHours(0, 0, 0, 0);
         return dateWithoutTime;
     });
@@ -197,12 +47,20 @@ export default function RecordListMonth() {
         return date.toLocaleDateString(undefined, options).toUpperCase();
     }
 
+    function getPaymentType(paymentType) {
+        if (paymentType === "CREDIT_CARD") {
+            return "Credit Card";
+        } else if (paymentType === "CASH") {
+            return "Cash";
+        }
+    }
+
     return (
         <>
             <div className="h-2"></div>
             {sortedUniqueRecordDates.map((recordDate) => {
-                const recordsForDate = recordsLastMonth.filter(record =>
-                    new Date(record.date).setHours(0, 0, 0, 0) === recordDate
+                const recordsForDate = records.filter(record =>
+                    new Date(record.dateAndTime).setHours(0, 0, 0, 0) === recordDate
                 );
                 return (
                     <Card key={recordDate} className="w-full shadow-lg mt-4">
@@ -215,39 +73,43 @@ export default function RecordListMonth() {
                             <div className="flow-root">
                                 <ul role="list" className="divide-y divide-gray-200">
 
-                                    {recordsForDate.map((recordForSpecificDate) => (
-                                        <li key={recordForSpecificDate.id} className="py-3 sm:py-4">
-                                            <Link>
-                                                <ListItem className="flex items-center space-x-4 text-left p-0 focus:bg-green-50 hover:bg-green-50">
-                                                    <div className="flex-shrink-0">
-                                                        <img className="w-8 h-8 rounded-full" src={recordForSpecificDate.category[0].icon} alt={recordForSpecificDate.category[0].categoryName} />
-                                                    </div>
-                                                    <div className="flex-1 min-w-0">
-                                                        <p className="text-sm font-medium text-gray-900 truncate">
-                                                            {recordForSpecificDate.category[0].categoryName}
-                                                        </p>
-                                                        <p className="text-sm text-gray-500 truncate">
-                                                            {recordForSpecificDate.paymentType}
-                                                        </p>
-                                                        <p className="text-sm text-gray-500 truncate">
-                                                            {recordForSpecificDate.note}
-                                                        </p>
-                                                    </div>
-                                                    <div className="text-right">
-                                                        <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                                                            -{recordForSpecificDate.amount.toLocaleString('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 2, maximumFractionDigits: 2})}
+                                    {recordsForDate.map((recordForSpecificDate) => {
+                                        let date = new Date(recordForSpecificDate.dateAndTime);
+                                        return (
+                                            <li key={recordForSpecificDate.id} className="py-3 sm:py-4">
+                                                <Link>
+                                                    <ListItem className="flex items-center space-x-4 text-left p-0 focus:bg-green-50 hover:bg-green-50">
+                                                        <div className="flex-shrink-0">
+                                                            <img className="w-8 h-8 rounded-full" src={recordForSpecificDate.category.icon.iconPath} alt={recordForSpecificDate.category.name} />
                                                         </div>
-                                                        <div className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                            {recordForSpecificDate.date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
+                                                        <div className="flex-1 min-w-0">
+                                                            <p className="text-sm font-medium text-gray-900 truncate">
+                                                                {recordForSpecificDate.category.name}
+                                                            </p>
+                                                            <p className="text-sm text-gray-500 truncate">
+                                                                {getPaymentType(recordForSpecificDate.paymentType)}
+                                                            </p>
+                                                            <p className="text-sm text-gray-500 truncate">
+                                                                {recordForSpecificDate.note}
+                                                            </p>
                                                         </div>
-                                                    </div>
-                                                    <div className="ml-2">
-                                                        <ChevronRightIcon className="h-5 w-5 text-green-800" />
-                                                    </div>
-                                                </ListItem>
-                                            </Link>
-                                        </li>
-                                    ))}
+                                                        <div className="text-right">
+                                                            <div className={`inline-flex items-center text-base font-semibold ${recordForSpecificDate.transactionType === 'EXPENSE' ? 'text-red-700' : 'text-green-700'}`}>
+                                                                {recordForSpecificDate.transactionType === 'EXPENSE' ? '- ' : '+ '}
+                                                                {recordForSpecificDate.amount.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                                                            </div>
+                                                            <div className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                                                {date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
+                                                            </div>
+                                                        </div>
+                                                        <div className="ml-2">
+                                                            <ChevronRightIcon className="h-5 w-5 text-green-800" />
+                                                        </div>
+                                                    </ListItem>
+                                                </Link>
+                                            </li>
+                                        );
+                                    })}
                                 </ul>
                             </div>
                         </CardBody>
