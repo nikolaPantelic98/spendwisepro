40fd5264ba4c5f6d6f246114f050da70d1b8cd60
nikola <nikolapantelic998@gmail.com>
2023-11-07 15:54:11 +0100

    Record repository created

---
+++ spendwisepro-web/spendwisepro-backend/spendwisepro-client/src/main/java/com/spendwisepro/client/record/RecordRepository.java
@@ -0,0 +1,6 @@
+package com.spendwisepro.client.record;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecordRepository extends JpaRepository<Record, Long> {
+}
