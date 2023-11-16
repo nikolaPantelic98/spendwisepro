3d43f5205b6cf3ab1e5e791717677715cc831682
nikola <nikolapantelic998@gmail.com>
2023-11-16 14:50:38 +0100

    Expense overview and graph for this month implemented

--- spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/expenses/ExpensesOverviewMonth.js
+++ spendwisepro-web/spendwisepro-frontend/spendwisepro-client-ui/src/components/expenses/ExpensesOverviewMonth.js
@@ -13,245 +13,27 @@ import {
     XAxis,
     YAxis
 } from "recharts";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {ChartBarIcon} from "@heroicons/react/24/solid";
+import axios from "axios";
 
 export default function ExpensesOverviewMonth() {
 
-    const records = [
-        {
-            id: 1,
-            amount: 70.00,
-            type: "expense",
-            date: new Date("2023-07-28T08:57"),
-            name: "Gas bill",
-            paymentType: "Credit Card",
-            category: [
-                { id: 1, categoryName: "Bills", color: "cyan", icon: "https://i.ibb.co/Y04MgVW/tax-icon-15117.png" }
-            ]
-        },
-        {
-            id: 2,
-            amount: 25.00,
-            type: "expense",
-            date: new Date("2023-07-28T12:30"),
-            name: "Phone bill",
-            paymentType: "Cash",
-            category: [
-                { id: 1, categoryName: "Bills", color: "cyan", icon: "https://i.ibb.co/Y04MgVW/tax-icon-15117.png" }
-            ]
-        },
-        {
-            id: 3,
-            amount: 45.00,
-            type: "expense",
-            date: new Date("2023-07-25T12:30"),
-            name: "Electricity bill",
-            paymentType: "Credit Card",
-            category: [
-                { id: 1, categoryName: "Bills", color: "cyan", icon: "https://i.ibb.co/Y04MgVW/tax-icon-15117.png" }
-            ]
-        },
-        {
-            id: 4,
-            amount: 15.00,
-            type: "expense",
-            date: new Date("2023-08-03T08:57"),
-            name: "Bread",
-            paymentType: "Cash",
-            category: [
-                { id: 2, categoryName: "Groceries", color: "yellow", icon: "https://icon-library.com/images/grocery-icon-png/grocery-icon-png-14.jpg" }
-            ]
-        },
-        {
-            id: 5,
-            amount: 27.00,
-            type: "expense",
-            date: new Date("2023-07-29T12:30"),
-            name: "Market",
-            paymentType: "Credit Card",
-            category: [
-                { id: 2, categoryName: "Groceries", color: "yellow", icon: "https://icon-library.com/images/grocery-icon-png/grocery-icon-png-14.jpg" }
-            ]
-        },
-        {
-            id: 6,
-            amount: 45.00,
-            type: "expense",
-            date: new Date("2023-07-29T12:30"),
-            name: "Tomato",
-            paymentType: "Credit Card",
-            category: [
-                { id: 2, categoryName: "Groceries", color: "yellow", icon: "https://icon-library.com/images/grocery-icon-png/grocery-icon-png-14.jpg" }
-            ]
-        },
-        {
-            id: 7,
-            amount: 100.00,
-            type: "expense",
-            date: new Date("2023-08-10T08:57"),
-            name: "Fuel",
-            paymentType: "Cash",
-            category: [
-                { id: 3, categoryName: "Fuel", color: "indigo", icon: "https://www.iconbunny.com/icons/media/catalog/product/1/2/1248.9-petrol-pump-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 8,
-            amount: 112.00,
-            type: "expense",
-            date: new Date("2023-08-10T12:30"),
-            name: "Fuel",
-            paymentType: "Credit Card",
-            category: [
-                { id: 3, categoryName: "Fuel", color: "indigo", icon: "https://www.iconbunny.com/icons/media/catalog/product/1/2/1248.9-petrol-pump-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 9,
-            amount: 45.00,
-            type: "expense",
-            date: new Date("2023-08-10T12:30"),
-            name: "Fuel",
-            paymentType: "Credit Card",
-            category: [
-                { id: 3, categoryName: "Fuel", color: "indigo", icon: "https://www.iconbunny.com/icons/media/catalog/product/1/2/1248.9-petrol-pump-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 10,
-            amount: 15.00,
-            type: "expense",
-            date: new Date("2023-07-22T08:57"),
-            name: "Cigarette",
-            paymentType: "Cash",
-            category: [
-                { id: 4, categoryName: "Tobacco", color: "blue-gray", icon: "https://www.iconbunny.com/icons/media/catalog/product/3/0/3050.12-cigarette-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 11,
-            amount: 27.00,
-            type: "expense",
-            date: new Date("2023-07-21T12:30"),
-            name: "Tobacco",
-            paymentType: "Credit Card",
-            category: [
-                { id: 4, categoryName: "Tobacco", color: "blue-gray", icon: "https://www.iconbunny.com/icons/media/catalog/product/3/0/3050.12-cigarette-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 12,
-            amount: 45.00,
-            type: "expense",
-            date: new Date("2023-08-16T12:30"),
-            name: "Pack",
-            paymentType: "Credit Card",
-            category: [
-                { id: 4, categoryName: "Tobacco", color: "blue-gray", icon: "https://www.iconbunny.com/icons/media/catalog/product/3/0/3050.12-cigarette-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 13,
-            amount: 15.00,
-            type: "expense",
-            date: new Date("2023-07-29T08:57"),
-            name: "Chips",
-            paymentType: "Cash",
-            category: [
-                { id: 5, categoryName: "Snacks", color: "light-green", icon: "https://www.iconbunny.com/icons/media/catalog/product/cache/2/thumbnail/600x/1b89f2fc96fc819c2a7e15c7e545e8a9/5/2/524.9-fries-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 14,
-            amount: 27.00,
-            type: "expense",
-            date: new Date("2023-07-25T12:30"),
-            time: "12:30",
-            name: "Chips",
-            paymentType: "Credit Card",
-            category: [
-                { id: 5, categoryName: "Snacks", color: "light-green", icon: "https://www.iconbunny.com/icons/media/catalog/product/cache/2/thumbnail/600x/1b89f2fc96fc819c2a7e15c7e545e8a9/5/2/524.9-fries-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 15,
-            amount: 45.00,
-            type: "expense",
-            date: new Date("2023-07-25T12:30"),
-            name: "Tortillas",
-            paymentType: "Credit Card",
-            category: [
-                { id: 5, categoryName: "Snacks", color: "light-green", icon: "https://www.iconbunny.com/icons/media/catalog/product/cache/2/thumbnail/600x/1b89f2fc96fc819c2a7e15c7e545e8a9/5/2/524.9-fries-icon-iconbunny.jpg" }
-            ]
-        },
-        {
-            id: 16,
-            amount: 15.00,
-            type: "expense",
-            date: new Date("2023-09-03T08:57"),
-            name: "Doctor",
-            paymentType: "Cash",
-            category: [
-                { id: 6, categoryName: "Health care", color: "green", icon: "https://cdn-icons-png.flaticon.com/512/206/206875.png" }
-            ]
-        },
-        {
-            id: 17,
-            amount: 27.00,
-            type: "expense",
-            date: new Date("2023-07-21T12:30"),
-            name: "Dentist",
-            paymentType: "Credit Card",
-            category: [
-                { id: 6, categoryName: "Health care", color: "green", icon: "https://cdn-icons-png.flaticon.com/512/206/206875.png" }
-            ]
-        },
-        {
-            id: 18,
-            amount: 45.00,
-            type: "expense",
-            date: new Date("2023-09-02T12:30"),
-            name: "Drug",
-            paymentType: "Credit Card",
-            category: [
-                { id: 6, categoryName: "Health care", color: "green", icon: "https://cdn-icons-png.flaticon.com/512/206/206875.png" }
-            ]
-        },
-        {
-            id: 19,
-            amount: 15.00,
-            type: "expense",
-            date: new Date("2023-07-21T08:57"),
-            name: "Card",
-            paymentType: "Cash",
-            category: [
-                { id: 7, categoryName: "Cinema", color: "deep-orange", icon: "https://cdn.imgbin.com/18/23/22/imgbin-computer-icons-clapperboard-film-coin-paQQD9iaFxMmALcBxefZD6Uv8.jpg" }
-            ]
-        },
-        {
-            id: 20,
-            amount: 27.00,
-            type: "expense",
-            date: new Date("2023-07-21T12:30"),
-            name: "Cinema chips",
-            paymentType: "Credit Card",
-            category: [
-                { id: 7, categoryName: "Cinema", color: "deep-orange", icon: "https://cdn.imgbin.com/18/23/22/imgbin-computer-icons-clapperboard-film-coin-paQQD9iaFxMmALcBxefZD6Uv8.jpg" }
-            ]
-        },
-        {
-            id: 21,
-            amount: 45.00,
-            type: "expense",
-            date: new Date("2023-09-01T12:30"),
-            name: "Coca cola",
-            paymentType: "Credit Card",
-            category: [
-                { id: 7, categoryName: "Cinema", color: "deep-orange", icon: "https://cdn.imgbin.com/18/23/22/imgbin-computer-icons-clapperboard-film-coin-paQQD9iaFxMmALcBxefZD6Uv8.jpg" }
-            ]
-        }
-    ];
+    const [expenseRecordsThisMonth, setExpenseRecordsThisMonth] = useState([]);
+
+    const token = localStorage.getItem("token");
+
+    const headers = {
+        'Authorization': `Bearer ${token}`
+    };
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/spendwisepro/records/expense_records_this_month', { headers })
+            .then(response => {
+                setExpenseRecordsThisMonth(response.data);
+            })
+            .catch(error => console.error('Error fetching expense records this month:', error));
+    }, []);
 
     const currentDate = new Date();
     const currentYear = currentDate.getFullYear();
@@ -265,19 +47,14 @@ export default function ExpensesOverviewMonth() {
     const endOfMonth = new Date(currentYear, currentMonth + 1, 0);
     endOfMonth.setHours(23, 59, 59, 999);
 
-    const expensesThisMonth = (() => {
-        return records.filter(record => {
-            return record.date >= startOfMonth && record.date <= endOfMonth && record.type === "expense";
-        });
-    })();
-
     // Generates data for a graph representing expenses for this month
     const expenseGraph = (() => {
         const dateToAmountMap = new Map();
 
         // Calculate total expenses for each date within this month
-        expensesThisMonth.forEach(record => {
-            const recordDate = record.date.toDateString();
+        expenseRecordsThisMonth.forEach(record => {
+            let date = new Date(record.dateAndTime);
+            const recordDate = date.toDateString();
             if (!dateToAmountMap.has(recordDate)) {
                 dateToAmountMap.set(recordDate, record.amount);
             } else {
@@ -322,7 +99,7 @@ export default function ExpensesOverviewMonth() {
     };
 
     // Calculate the total amount spent on expenses within this month.
-    const totalAmountThisMonth = expensesThisMonth.reduce((total, record) => total + record.amount, 0);
+    const totalAmountThisMonth = expenseRecordsThisMonth.reduce((total, record) => total + record.amount, 0);
 
     return (
         <Card className="w-full shadow-lg mt-8">
