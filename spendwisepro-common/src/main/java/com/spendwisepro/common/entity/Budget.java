7b2713e426ec5a908ac0fb939c6643f268daa4d5
nikola <nikolapantelic998@gmail.com>
2023-11-27 16:41:59 +0100

    Budget entity created

---
+++ spendwisepro-common/src/main/java/com/spendwisepro/common/entity/Budget.java
@@ -0,0 +1,58 @@
+package com.spendwisepro.common.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Table(
+        name = "budgets"
+)
+public class Budget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            nullable = false
+    )
+    private Float amount;
+
+    @Column(
+            length = 128,
+            nullable = false
+    )
+    private String name;
+
+    @Enumerated(
+            EnumType.STRING
+    )
+    private BudgetPeriod period;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "budgets_categories",
+            joinColumns = @JoinColumn(
+                    name = "budget_id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "category_id"
+            )
+    )
+    private Set<Category> categories = new LinkedHashSet<>();
+
+    @ManyToOne
+    @JoinColumn(
+            name = "user_id"
+    )
+    private User user;
+}
